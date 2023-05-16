368a03ff182f3eaf72de42fbb8df61d95116be0e
kayaselin <selinkaya366@gmail.com>
2023-05-16 19:27:06 +0200

    6. kısım

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
 @extends('layout')
 
 @section('title', 'Home')
 
 @section('content')
-<h1>Home page</h1>
-<p>This is the home page.</p>
+@forelse ($posts as $post)
+<div class="post-item">
+  <div class="post-content">
+    <h2>{{ $post->title }}</h2>
+    <p>{{ $post->description }}</p>
+  </div>
+</div>
+@empty
+<h2>There are no posts yet.</h2>
+@endforelse
 @endsection
